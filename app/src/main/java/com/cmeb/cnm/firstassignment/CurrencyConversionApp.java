aef3622ad2c70b5b94522d0c36ec65f29019efab
carlamota <carla.n.mota@gmail.com>
2017-11-09 15:54:34 +0000

    Initial commit

---
+++ app/src/main/java/com/cmeb/cnm/firstassignment/CurrencyConversionApp.java
@@ -0,0 +1,13 @@
+package com.cmeb.cnm.firstassignment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CurrencyConversionApp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency_conversion_app);
+    }
+}
