a7e376f582e18740e30f4fa7a6f7f9e95cfea4f0
carlamota <carla.n.mota@gmail.com>
2017-11-14 23:52:27 +0000

    test1

--- app/src/main/java/com/cmeb/cnm/firstassignment/Taxes_check.java
+++ app/src/main/java/com/cmeb/cnm/firstassignment/Taxes_check.java
@@ -1,14 +1,35 @@
 package com.cmeb.cnm.firstassignment;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 /**
  * Created by User on 14/11/2017.
  */
 
-public class Taxes_check extends CurrencyConversionApp{
+public class Taxes_check extends CurrencyConversionApp {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.taxes_layout);
+
+        // Spinner element
+        Spinner spinner_taxes = (Spinner) findViewById(R.id.spinner);
+
+
+        // Spinner click listener
+
+//        for(int i = 0; i < 30; i++)
+//        {
+//
+//
+//            Button b = new Button(this);
+//            b.setText("Button "+i);
+//            ll.addView(b);
+//        }
+
+
     }
 }
